d9ef19a33f9e455834ea01a1084c4fce03c374e1
Solaror0 <133717438+Solaror0@users.noreply.github.com>
2024-08-03 00:23:36 -0400

    preparingModelTraining(flaskAPI.py)

--- flaskAPI.py
+++ flaskAPI.py
@@ -2,13 +2,17 @@ from flask import app, Flask, redirect, url_for, request, render_template, flash
 import os
 from werkzeug.utils import secure_filename
 import jsonify
-import model.keras
+import model.py
+import cv2
+from keras import load_model
+from keras import image
 
 app = Flask(__name__)
 
 UPLOAD_FOLDER = 'uploads/'
 ALLOWED_EXTENSIONS = {'mp4', 'mov', 'avi', 'mkv'}  # Add other video formats as needed
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+listOfNames = ["andy","james","josh","junnur"]
 
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
@@ -16,8 +20,16 @@ if not os.path.exists(UPLOAD_FOLDER):
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+def liveVideoInput():
+    kmodel = load_model("model.keras")
+    frame = cv2.VideoCapture(0)
+    kmodel.predict(frame)
+    model.fetchName()
+  
+
 @app.route('/')
 def default():
+
     return render_template('welcome.html')
 
 @app.route('/upload', methods=['POST'])
@@ -41,8 +53,9 @@ def upload_file():
 
 @app.route('/train')
 def train():
+   #  list.append(name)
     model.keras.train()
-    
+
 
 
 if __name__ == '__main__':
